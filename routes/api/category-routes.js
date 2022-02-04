18945f08db8b3878002aa0a2ddbfe0114a62b292
Justin Garner <j.garner828@gmail.com>
2022-02-04 09:10:49 -0500

    added DELETE to /api/categories/:id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,7 +34,7 @@ router.post('/', async (req, res) => {
   try {
     const newCategory = await Category.create({category_name: req.body.category_name})
     res.status(200).json(newCategory);
-    
+
   } catch (err) {
     res.status(500).json(err);
   }
@@ -52,6 +52,12 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
+    const deleteCatById = await Category.destroy({where: {id: req.params.id}});
+    if(!deleteCatById) {
+      res.status(404).json({message: 'No category with this Id'});
+      return;
+    }
+    res.status(200).json(deleteCatById);
 
   } catch (err) {
     res.status(500).json(err);
