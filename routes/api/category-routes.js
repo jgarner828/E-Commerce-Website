d2b745e2785489eae21578abef70c51413ef31ec
Justin Garner <j.garner828@gmail.com>
2022-02-04 09:29:10 -0500

    created PUT for /api/categories/:id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,6 +44,9 @@ router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
 
+    const updateCategory = await Category.update({category_name: req.body.category_name}, {where: {id: req.params.id}});
+    res.status(200).json(updateCategory)
+
   } catch (err) {
     res.status(500).json(err);
   }
